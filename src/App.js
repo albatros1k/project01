372e745b965b6e1580a285e5f35bf2ef658fe320
Albert <superalbertem@gmail.com>
2020-12-20 14:24:22 +0300

    Finish app component JSX

--- src/App.js
+++ src/App.js
@@ -6,17 +6,34 @@ const App = () => {
   return (
 <div className = 'app-wrapper'>
   <header className ='header'>
-    <img src='https://i.pinimg.com/600x315/81/2c/bc/812cbc131d571f0071b66dda400bad00.jpg'/>
+    <img src='https://iconape.com/wp-content/png_logo_vector/albatros-de-brest-logo.png'/>
   </header>
   <nav className = 'nav'>
     <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
+      <li><a href="" className="">Profile</a></li>
+      <li><a href="" className="">Messages</a></li>
+      <li><a href="" className="">News</a></li>
+      <li><a href="" className="">Music</a></li>
+      <li><a href="" className="">Settings</a></li>
     </ul>
   </nav>
   <div className = 'content'>
+    <div>
+      <img src="https://content.presspage.com/uploads/2379/1920_ocean-sunset-195865.jpg?10000"/>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      my posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>post1</div>
+        <div>post2</div>
+      </div>
+    </div>
     Main content
     </div>
 </div>
