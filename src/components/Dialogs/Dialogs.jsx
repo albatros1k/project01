e47efaec5721346bc16b41257f37585c452b9671
Albert <superalbertem@gmail.com>
2020-12-21 18:07:20 +0300

    BLL and UI

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import s from "./Dialogs.module.css";
+
 const DialogItem = (props)=>{
 		let path = `/dialogs/${props.id}`
 		return (
@@ -15,10 +16,26 @@ const Message = (props)=>{
 		)
 	}
 const Dialogs = (props) =>{
+	let dialogsData = [
+	{id: 1, name:'Albert'},
+	{id: 2, name:'Viktoria'},
+	{id: 3, name:'Viktor'},
+	{id: 4, name:'Tatyana'},
+	{id: 5, name:'Evgeniy'},
+	{id: 6, name:'Kurul'},
+];
+	let messagesData = [
+	{id: 1, message:'Hello'},
+	{id: 2, message:'How are you?'},
+	{id: 3, message:'What about your development?'},
+	{id: 4, message:'Yo'},
+	{id: 5, message:'Yo'},
+	{id: 6, message:'Yo'},
+];
 		return (
 			<div className={s.dialogs}>
 				<section className={s.dialogItems}>
-			<DialogItem name='Albert' id='1'/>
+			<DialogItem name={dialogsData[0].name} id={dialogsData.[0].id}/>
 			<DialogItem name='Viktoria' id='2'/>
 			<DialogItem name='Viktor' id='3'/>
 			<DialogItem name='Tatyana' id='4'/>
@@ -26,7 +43,7 @@ const Dialogs = (props) =>{
 			<DialogItem name='Kurul' id='6'/>
 				</section>
 				<section className={s.messages}>
-					<Message message='Hello'/>
+					<Message message={messagesData[0].message}/>
 					<Message message='How are you?'/>
 					<Message message='What about your development?'/>
 				</section>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 const MyPosts = () =>{
+  let postData =[
+    {id:1, message:'Hi how are you',likesCount:12},
+    {id:1, message:'It is my first post',likesCount:11},
+  ]
 	return (
 <div className='postsBlock'>
       my posts
@@ -10,8 +14,8 @@ const MyPosts = () =>{
         <button className={s.button}>Add post</button>
       </div>
       <div className = {s.posts}>
-    <Post message='Hi how are you' likesCount = '0'/>
-    <Post message ='It is my first post' likesCount ='23'/>
+    <Post message={postData[0].message} likesCount = {postData[0].likesCount}/>
+    <Post message ={postData[1].message} likesCount ={postData[1].likesCount}/>
       </div>
     </div>
 	)
