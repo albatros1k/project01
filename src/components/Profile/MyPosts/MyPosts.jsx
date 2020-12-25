b8d7390eabba08b7398b054be062f8d0930170ca
Albert <superalbertem@gmail.com>
2020-12-25 16:54:38 +0300

    Created dispatch

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,15 +7,13 @@ const MyPosts = (props) => {
   });
 
   let newPostElement = React.createRef();
-
   const addPost = () => {
-    let text = newPostElement.current.value;
-    props.addPost();
-    props.updateNewPostText('');
+    props.dispatch({ type: "ADD-POST" });
   };
+
   const onPostChange = () => {
     let text = newPostElement.current.value;
-    props.updateNewPostText(text);
+    props.dispatch({ type: "UPDATE-NEW-POST-TEXT", newText: text });
   };
 
   return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,65 +1,69 @@
 let store = {
-  _state : {
-  profilePage: {
-    posts: [
-      { id: 1, message: "Hi how are you", likesCount: 12 },
-      { id: 2, message: "It is my first post", likesCount: 11 },
-    ],
-    newPostText: "New post",
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: "Hi how are you", likesCount: 12 },
+        { id: 2, message: "It is my first post", likesCount: 11 },
+      ],
+      newPostText: "New post",
+    },
+    dialogsPage: {
+      messages: [
+        { id: 1, message: "Hello" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "What about your development?" },
+        { id: 4, message: "Yo" },
+        { id: 5, message: "Yo" },
+        { id: 6, message: "Yo" },
+      ],
+      dialogs: [
+        { id: 1, name: "Albert" },
+        { id: 2, name: "Viktoria" },
+        { id: 3, name: "Viktor" },
+        { id: 4, name: "Tatyana" },
+        { id: 5, name: "Evgeniy" },
+        { id: 6, name: "Kurul" },
+      ],
+    },
   },
-  dialogsPage: {
-    messages: [
-      { id: 1, message: "Hello" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "What about your development?" },
-      { id: 4, message: "Yo" },
-      { id: 5, message: "Yo" },
-      { id: 6, message: "Yo" },
-    ],
-    dialogs: [
-      { id: 1, name: "Albert" },
-      { id: 2, name: "Viktoria" },
-      { id: 3, name: "Viktor" },
-      { id: 4, name: "Tatyana" },
-      { id: 5, name: "Evgeniy" },
-      { id: 6, name: "Kurul" },
-    ],
+  getState() {
+    return this._state;
   },
-},
-getState() {
-  return this._state;
-},
-_callSubscriber () {
-  console.log("State is changed");
-},
-addPost () {
-  let newPost = {
-    id: 5,
-    message: this._state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  this._state.profilePage.posts.push(newPost);
-  this._callSubscriber(this._state);
-},
-updateNewPostText (newText) {
-  this._state.profilePage.newPostText = newText;
-  this._callSubscriber(this._state);
-},
-subscribe (observer) {
-  this._callSubscriber = observer;
-},
-}
-
-
-
-
-
-
-
-
-
-
+  _callSubscriber() {
+    console.log("State is changed");
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
+  dispatch(action) {
+    if (action.type === 'ADD-POST') {
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0,
+      };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    }
+  },
+};
 
 export default store;
 window.store = store;
-
