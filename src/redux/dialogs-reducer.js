7f73c6d97d3719170cb70d501217110c9e1da046
Albert <superalbertem@gmail.com>
2020-12-27 16:45:52 +0300

    Redux practice and combineReducers

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,27 @@
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE_NEW_MESSAGE_BODY";
 const SEND_MESSAGE = "SEND_MESSAGE";
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+      messages: [
+        { id: 1, message: "Hello" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "What about your development?" },
+        { id: 4, message: "Yo" },
+        { id: 5, message: "Yo" },
+        { id: 6, message: "Yo" },
+      ],
+      dialogs: [
+        { id: 1, name: "Albert" },
+        { id: 2, name: "Viktoria" },
+        { id: 3, name: "Viktor" },
+        { id: 4, name: "Tatyana" },
+        { id: 5, name: "Evgeniy" },
+        { id: 6, name: "Kurul" },
+      ],
+      newMessageBody: "",
+    };
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
       state.newMessageBody = action.body;
@@ -28,4 +48,5 @@ export const updateNewMessageBodyCreator = (body) => {
     body: body,
   };
 };
+
 export default dialogsReducer;
