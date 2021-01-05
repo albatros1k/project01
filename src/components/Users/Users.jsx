96098d12b3de71b2206291b277c6bdca61eca94f
Albert <superalbertem@gmail.com>
2021-01-05 17:28:27 +0300

    Pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,20 +4,53 @@ import s from "./Users.module.css";
 import userPhoto from "../../assets/images/user.png";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
-    if (this.props.users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((res) => {
-          this.props.setUsers(res.data.items);
-        });
-    }
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((res) => {
+        this.props.setUsers(res.data.items);
+      });
   }
 
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((res) => {
+        this.props.setUsers(res.data.items);
+        this.props.setTotalUsersCount(res.data.totalCount);
+      });
+  };
   render() {
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+    console.log(pagesCount);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+
     return (
       <div className={s.users}>
+        <div className={s.pages}>
+          {pages.map((p) => {
+            return (
+              <span
+                className={this.props.currentPage === p && s.selectedPage}
+                onClick={(e) => {
+                  this.onPageChanged(p);
+                }}
+              >
+                {p}
+              </span>
+            );
+          })}
+        </div>
         {this.props.users.map((u) => (
           <div key={u.id}>
             <span>
@@ -57,7 +90,7 @@ class Users extends React.Component {
                   <div>{u.status}</div>
                 </span>
                 <span>
-                  <div>{"u.location.city"}</div>
+                  <div>id {u.id}</div>
                   <div>{"u.location.country"}</div>
                 </span>
               </div>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,10 +1,19 @@
 import { connect } from "react-redux";
-import { setUsersAC, unfollowAC, followAC } from "../../redux/users-reducer";
-import Users from './Users'
+import {
+  setUsersAC,
+  unfollowAC,
+  followAC,
+  setCurrentPageAC,
+  setUsersTotalCountAC,
+} from "../../redux/users-reducer";
+import Users from "./Users";
 
 const mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
+    pageSize: state.usersPage.pageSize,
+    totalUsersCount: state.usersPage.totalUsersCount,
+    currentPage: state.usersPage.currentPage,
   };
 };
 
@@ -18,6 +27,12 @@ const mapDispatchToProps = (dispatch) => {
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users));
+    },
+    setCurrentPage: (pageNumber) => {
+      dispatch(setCurrentPageAC(pageNumber));
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setUsersTotalCountAC(totalCount));
     },
   };
 };
